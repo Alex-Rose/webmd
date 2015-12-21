59d68f6657cd9f17305e625cd58d8afac55bbd3a
Alexandre Rose <alexandre.rose@polymtl.ca>
2015-12-21 11:27:18 -0500

    Add multiple themes

---
+++ style.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    if (window.localStorage) {
+        var theme = localStorage.getItem('theme');
+        if (theme && theme != 'default') {
+            changeTheme(theme);
+        }
+    }
+});
+
+function changeTheme(url) {
+    $('#theme').attr('href', url)
+}
+
+$(document).on('click', 'a.theme', function(e) {
+    e.preventDefault();
+    var url = $(this).attr('href');
+    changeTheme(url);
+    
+    if (window.localStorage) {
+        if ($(this).hasClass('default')) {
+            url = 'default';
+        }
+        
+        localStorage.setItem('theme', url);
+    }
+});
