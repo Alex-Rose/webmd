86299c18840accaf9ce4c01f196a0861f7689037
Alexandre Rose <alexandre.rose@polymtl.ca>
2015-12-22 12:59:35 -0500

    Script to generate index of all available files

---
+++ generate_index.php
@@ -0,0 +1,31 @@
+<?php
+$directory = __DIR__.'/content';
+$filename = $directory.'/content.md';
+$files = [];
+
+foreach ($iterator = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator($directory,
+        RecursiveDirectoryIterator::SKIP_DOTS),
+        RecursiveIteratorIterator::SELF_FIRST) as $item) 
+{
+    if (!$item->isDir())
+    {
+        $name = $iterator->getSubPathName();
+        if ($name == '.htaccess') continue;
+        
+        array_push($files, $name);
+    }
+}
+
+var_dump($files);
+
+$content = "# Index \n\n";
+$content .= "Voici la liste de tous les fichiers .md dans le répertoire content. 
+            Ce fichier est auto généré par le script *generate_index.php*\n\n";
+            
+foreach ($files as $file) 
+{
+    $content .= '* ['.$file.']('.$file.")\n";
+}
+
+file_put_contents($filename, $content);
